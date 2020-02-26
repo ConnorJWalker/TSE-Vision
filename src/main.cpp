357f380760bea93905d077bd156a042d359fb988
Connor Walker <connorjwalker32@hotmail.co.uk>
2020-02-26 18:22:47 +0000

    added argument reader

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,36 @@
-#include <opencv.h>
+#include <string>
+#include <cstring>
+#include <iostream>
 
-int main() {
-    cv::Mat image(200, 200, CV_8UC1, cv::Scalar(0, 0, 255));
+/*
+ * Start our application by passing in the output filepath for card data and a flag
+ * for if we should render to screen. These will be stored in argv array as such:
+ *
+ * argv[0] - Application name
+ * argv[1] - Filepath to detected card output file
+ * argv[2] - Should render to screen for debug (optional, default to false)
+ */
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        std::cerr << "No arguments have been entered\n" << std::endl;
+        std::cerr << "Please use the following format: ./vision.exe <card output filepath> [<-r>]" << std::endl;
 
-    imshow("Test", image);
-
-    while (true) {
-        if (cv::waitKey(30) >= 0) break;
+        return 1;
     }
+
+    std::string outputFilepath = argv[1];
+
+    bool shouldRender = false;
+    if (argc == 3) {
+        if (std::strcmp("-r", argv[2]) == 0) {
+            shouldRender = true;
+        }
+    }
+    else if (argc > 3) {
+        std::cout << "More than 2 arguments have been entered, extra arguments have been ignored" << std::endl;
+    }
+
+    std::cout << "Filepath: " << outputFilepath << " Should Render: " << (shouldRender ? "true" : "false") << std::endl;
+
+    return 0;
 }
