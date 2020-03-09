ca457b8c05c250cb357de0d66684457ee03cc373
NoahChar <43756351+NoahChar@users.noreply.github.com>
2020-03-09 13:11:57 +0000

    Added colour detection

--- src/vision/detector.cpp
+++ src/vision/detector.cpp
@@ -13,7 +13,33 @@ void Detector::detectIfFaceUp(cv::Mat image, cv::Rect roi) {
 }
 
 void Detector::detectColour(cv::Mat hsvImage, cv::Rect roi) {
+    cv::Mat Roi(hsvImage, roi);
+	double redPrecentage, blackPercentage;
+	std::vector<int> lowerRed = { 0, 120, 70 };
+	std::vector<int> upperRed = { 10, 255, 255 };
+	std::vector<int> lowerBlack = { 0, 0, 0 };
+	std::vector<int> upperBlack = { 180, 255, 125 };
+	auto colourDetection = [](double& percentage, cv::Mat roi, std::vector<int>upperBound, std::vector<int>lowerBound) {
+		cv::Mat mask;
+		cv::inRange(roi, lowerBound, upperBound, mask);
 
+		int imageSize = mask.rows * mask.cols;
+		cv::threshold(mask, mask, 120, -1, cv::THRESH_TOZERO);
+
+		percentage = 1 - (imageSize - cv::countNonZero(mask)) / (double)imageSize;
+	};
+	
+	colourDetection(redPrecentage, Roi, upperRed, lowerRed);
+
+	
+	colourDetection(blackPercentage, Roi, upperBlack, lowerBlack);
+
+	if (redPrecentage > blackPercentage) {
+		
+		Colour::Red;
+	}
+		
+	Colour::Black;
 }
 
 void Detector::detectCardValue(cv::Mat image, cv::Rect roi) {
