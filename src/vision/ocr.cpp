903c13b78ea905803e1008db65802380f25573da
Connor Walker <connorjwalker32@hotmail.co.uk>
2020-04-20 18:09:44 +0100

    added set up and clean up code for tesseract

--- src/vision/ocr.cpp
+++ src/vision/ocr.cpp
@@ -1,11 +1,19 @@
 #include "ocr.h"
 
 Ocr::Ocr() {
+    ocr = new tesseract::TessBaseAPI();
 
+    ocr->Init(NULL, "eng", tesseract::OEM_LSTM_ONLY);
+    ocr->SetPageSegMode(tesseract::PSM_SINGLE_CHAR);
+
+    ocr->SetVariable("tessedit_char_whitelist", "A0123456789JQK");
+    ocr->SetVariable("load_system_dawg", "false");
+    ocr->SetVariable("load_freq_dawg", "false");
 }
 
 Ocr::~Ocr() {
-
+    ocr->End();
+    delete ocr;
 }
 
 DetectedCard Ocr::run(cv::Mat roi, DetectedCard card) {
